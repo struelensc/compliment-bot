08d674c73ad15f95f72cc3c2eb835bcc3c91ef1f
Courtney Struelens <courtneystruelens@hotmail.com>
2024-08-20 19:58:53 -0700

    fixed bug in server.js file

--- commands/utility/server.js
+++ commands/utility/server.js
@@ -4,8 +4,8 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("server")
     .setDescription("Provides information about the server."),
-  async execute(iinteraction) {
-    await InteractionCollector.reply(
+  async execute(interaction) {
+    await interaction.reply(
       `This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`
     );
   },
